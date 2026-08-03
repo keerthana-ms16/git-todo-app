19e095f2b7ad793f2c561da91e51191ba2e209a8
keerthana-ms16 <keerthana.ms@ecanarys.com>
2026-08-03 11:38:50 +0530

    Initial Commit - created git-todo-application

---
+++ app.py
@@ -0,0 +1,52 @@
+tasks = []
+
+
+def add_task():
+    task = input("Enter task: ")
+    tasks.append(task)
+    print("Task added successfully!")
+
+
+def view_tasks():
+    if len(tasks) == 0:
+        print("No tasks available")
+    else:
+        print("\nYour Tasks:")
+        for i, task in enumerate(tasks, 1):
+            print(f"{i}. {task}")
+
+
+def delete_task():
+    view_tasks()
+
+    if len(tasks) > 0:
+        number = int(input("Enter task number to delete: "))
+        tasks.pop(number - 1)
+        print("Task deleted successfully!")
+
+
+while True:
+
+    print("\n===== TO-DO APPLICATION =====")
+    print("1. Add Task")
+    print("2. View Tasks")
+    print("3. Delete Task")
+    print("4. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        add_task()
+
+    elif choice == "2":
+        view_tasks()
+
+    elif choice == "3":
+        delete_task()
+
+    elif choice == "4":
+        print("Thank you!")
+        break
+
+    else:
+        print("Invalid choice")
